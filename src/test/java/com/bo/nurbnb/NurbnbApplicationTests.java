879a7531953972915ab2522b7f4724190e9b7f7c
Cristhian Vargas Quiroz <cristhianingsis@gmail.com>
2024-01-09 07:25:26 -0400

    first commit

---
+++ src/test/java/com/bo/nurbnb/NurbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.nurbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
